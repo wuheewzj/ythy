d043018105fef33413e022650f9acb515f4fcc09
wuheewzj <wuheewzj@163.com>
2020-09-05 20:14:54 +0800

    初始化版本

---
+++ src/main/java/com/zsyl/ythy/YthyApplication.java
@@ -0,0 +1,13 @@
+package com.zsyl.ythy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YthyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YthyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zsyl/ythy/YthyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsyl.ythy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YthyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
